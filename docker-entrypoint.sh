4052f5d9bacbbc73795a82da8db3a624043b1c64
Diego Daniel Caceres <diegodanielcaceres10@gmail.com>
2026-07-29 23:56:41 +0200

    fix: docker node image for runing app on docker container

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 # Exit immediately if a command exits with a non-zero status
 set -e
-echo "Starting Nura frontend..."
+echo "Starting RIU frontend..."
+
+if ! node -e "require('@rollup/rollup-linux-x64-gnu')" >/dev/null 2>&1; then
+  echo "Installing Linux dependencies..."
+  npm ci
+fi
+
 exec npm start
