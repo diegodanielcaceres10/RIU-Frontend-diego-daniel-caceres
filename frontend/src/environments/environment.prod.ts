996767500c42ea9de7a9a7472ea66c563976ec69
Diego Daniel Caceres <diegodanielcaceres10@gmail.com>
2026-07-31 15:51:38 +0200

    fixing mockUrl json on production environment for test on githubpages

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   useMockData: false,
   apiUrl: 'https://mi-api-real.com/api',
-  mockUrl: '/assets/mock-data',
+  mockUrl: '/RIU-Frontend-diego-daniel-caceres/assets/mock-data',
 };
